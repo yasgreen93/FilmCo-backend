e3fd9cdde25d7246a615003302a7ec52382aea16
Yasmin Green <yasmingreen@Yasmins-MBP.home>
2016-05-27 18:11:05 +0100

    Added header type

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,7 +8,7 @@ var associateTag = process.env.AWS_ASSOCIATE_TAG;
 
 router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, dataType");
   next();
 });
 
