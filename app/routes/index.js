91da0fe97b6162bbd0c1b020722accc29824bc96
Yasmin Green <yasmingreen@Yasmins-MBP.home>
2016-05-27 13:15:02 +0100

    Added aws-lib, api call working

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var aws = require("../../node_modules/aws-lib/lib/aws");
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
+});
+
+router.get('/films/api', function(req, res, next) {
+  var prodAdv = aws.createProdAdvClient(yourAccessKeyId, yourSecretAccessKey, yourAssociateTag);
+  var options = {SearchIndex: "DVD", Keywords: req.barcodeNum};
+
+  return prodAdv.call("ItemSearch", options, function(err, result) {
+    console.log(result.Items.Item.ItemAttributes.Title);
+    res.json(result);
+  });
 });
 
 module.exports = router;
